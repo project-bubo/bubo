a74fcabf7fa52e9179fbde0d3fda437b758fc868
Marek Juras <marek.juras@lundegaard.eu>
2013-08-10 18:56:52 +0200

    updated PresenterFactory, introduce BuboApp namespace to Media component

--- src/Bubo/Application/PresenterFactory.php
+++ src/Bubo/Application/PresenterFactory.php
@@ -2,6 +2,8 @@
 
 namespace Bubo\Application;
 
+use Nette;
+use Nette\DI\IContainer;
 use Nette\Application\PresenterFactory as NettePresenterFactory;
 
 /**
@@ -11,26 +13,53 @@ use Nette\Application\PresenterFactory as NettePresenterFactory;
 class PresenterFactory extends NettePresenterFactory
 {
 
-	/**
-	 * Formats presenter class name from its name.
-	 * @param  string
-	 * @return string
-	 */
-	public function formatPresenterClass($presenter)
-	{
-		return str_replace(':', 'Module\\Presetners\\', $presenter) . 'Presenter';
-	}
+    protected $appNamespace = 'BuboApp';
+
+    /**
+     * Constructor
+     * @param string $baseDir
+     * @param Nette\ComponentModel\Container $container
+     */
+    public function __construct($baseDir, Nette\DI\Container $container)
+    {
+            parent::__construct($baseDir, $container);
+    }
+
+    /**
+     * Formats presenter class name from its name.
+     * @param  string
+     * @return string
+     */
+    public function formatPresenterClass($presenter)
+    {
+        $chunks = explode(':', $presenter);
+        $presenterName = array_pop($chunks) . 'Presenter';
+        $class = $this->appNamespace .
+                 '\\' . str_replace(':', 'Module\\', implode(':', $chunks) . ':') .
+                 $presenterName;
+        return $class;
+    }
 
 
 
-	/**
-	 * Formats presenter name from class name.
-	 * @param  string
-	 * @return string
-	 */
-	public function unformatPresenterClass($class)
-	{
-		return str_replace('Module\\Presenters\\', ':', substr($class, 0, -9));
-	}
+    /**
+     * Formats presenter name from class name.
+     * @param  string
+     * @return string
+     */
+    public function unformatPresenterClass($class)
+    {
+            return substr(str_replace('Module\\', ':', substr($class, 0, -9)), 8);
+    }
+
+    /**
+     * @param \Nette\DI\IContainer $container
+     * @return \Bubo\Application\PresenterFactory
+     */
+    public static function factory(IContainer $container)
+    {
+        $appDir = $container->parameters['appDir'];
+        return new PresenterFactory($appDir, $container);
+    }
 
 }
